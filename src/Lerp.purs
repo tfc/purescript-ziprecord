14dde00fd3606d8d668113979e97d56cd278e920
Jacek Galowicz <jacek@galowicz.de>
2023-01-26 13:57:45 +0100

    Rename to lerp

---
+++ src/Lerp.purs
@@ -0,0 +1,82 @@
+module Lerp where
+
+import Prelude
+
+import Prim.Row as Row
+import Prim.RowList as RL
+import Record (get) as R
+import Type.Prelude (class IsSymbol)
+import Type.Proxy (Proxy(..))
+import Record.Builder (Builder)
+import Record.Builder as Builder
+import Data.Int (toNumber, round)
+
+class Lerp a where
+  lerp :: Number -> a -> a -> a
+
+instance lerpNumber :: Lerp Number where
+  lerp n a b = a + (b - a) * n
+
+instance lerpInt :: Lerp Int where
+  lerp n a b = round $ lerp n (toNumber a) (toNumber b)
+
+instance lerpBoolean :: Lerp Boolean where
+  lerp n a b = roundBool $ lerp n (fromBool a) (fromBool b)
+    where
+    fromBool = if _ then 1.0 else 0.0
+    roundBool x = round x > 0
+
+class
+  LerpRecord
+    (rl :: RL.RowList Type)
+    (r :: Row Type)
+    (from :: Row Type)
+    (to :: Row Type)
+  | rl -> r from to
+  where
+  lerpRecordImpl
+    :: Number
+    -> Proxy rl
+    -> Record r
+    -> Record r
+    -> Builder { | from } { | to }
+
+instance lerpRecordNil :: LerpRecord RL.Nil trashA () () where
+  lerpRecordImpl _ _ _ _ = identity
+
+instance lerpRecordCons ::
+  ( IsSymbol k
+  , Row.Cons k a trashA r
+  , Row.Cons k a from' to
+  , Row.Lacks k from'
+  , LerpRecord t r from from'
+  , Lerp a
+  ) =>
+  LerpRecord (RL.Cons k a t) r from to where
+  lerpRecordImpl n _ a b = current <<< next
+    where
+    current = Builder.insert name head
+    next = lerpRecordImpl n proxyA a b
+    name = Proxy :: _ k
+    head = lerp n (R.get name a) (R.get name b)
+    proxyA = Proxy :: _ t
+
+lerpRecord
+  :: forall t r
+   . RL.RowToList r t
+  => LerpRecord t r () r
+  => Number
+  -> Record r
+  -> Record r
+  -> Record r
+lerpRecord n a b = Builder.build builder {}
+  where
+  proxyA = Proxy :: _ t
+  builder = lerpRecordImpl n proxyA a b
+
+instance lerpRecordInstance ::
+  ( RL.RowToList a t
+  , LerpRecord t a () a
+  ) =>
+  Lerp (Record a) where
+  lerp = lerpRecord
--- test/Main.purs
+++ test/Main.purs
@@ -8,28 +8,28 @@ import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Interpolate (interpolate)
+import Lerp (lerp)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
-  describe "Interpolate class" do
-    it "correctly interpolates ints" do
-      interpolate 0.0 0 10 `shouldEqual` 0
-      interpolate 0.1 0 10 `shouldEqual` 1
-      interpolate 0.5 0 10 `shouldEqual` 5
-      interpolate 1.0 0 10 `shouldEqual` 10
-    it "correctly interpolates Floats" do
-      interpolate 0.0 0.0 10.0 `shouldEqual` 0.0
-      interpolate 0.5 0.0 10.0 `shouldEqual` 5.0
-      interpolate 1.0 0.0 10.0 `shouldEqual` 10.0
-    it "correctly interpolates Booleans" do
-      interpolate 0.0 false true `shouldEqual` false
-      interpolate 0.1 false true `shouldEqual` false
-      interpolate 0.5 false true `shouldEqual` true
-      interpolate 1.0 false true `shouldEqual` true
-    it "correctly interpolates non-nested records" do
-      interpolate 0.5 { a: 0 } { a: 10 } `shouldEqual` { a: 5 }
-    it "correctly interpolates nested records" do
+  describe "Lerp class" do
+    it "correctly lerps ints" do
+      lerp 0.0 0 10 `shouldEqual` 0
+      lerp 0.1 0 10 `shouldEqual` 1
+      lerp 0.5 0 10 `shouldEqual` 5
+      lerp 1.0 0 10 `shouldEqual` 10
+    it "correctly lerps Floats" do
+      lerp 0.0 0.0 10.0 `shouldEqual` 0.0
+      lerp 0.5 0.0 10.0 `shouldEqual` 5.0
+      lerp 1.0 0.0 10.0 `shouldEqual` 10.0
+    it "correctly lerps Booleans" do
+      lerp 0.0 false true `shouldEqual` false
+      lerp 0.1 false true `shouldEqual` false
+      lerp 0.5 false true `shouldEqual` true
+      lerp 1.0 false true `shouldEqual` true
+    it "correctly lerps non-nested records" do
+      lerp 0.5 { a: 0 } { a: 10 } `shouldEqual` { a: 5 }
+    it "correctly lerps nested records" do
       let
         a =
           { a: 0.0
@@ -55,4 +55,4 @@ main = launchAff_ $ runSpec [ consoleReporter ] do
               , e: true
               }
           }
-      interpolate 0.7 a b `shouldEqual` c
+      lerp 0.7 a b `shouldEqual` c
